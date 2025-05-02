b55872056fd9f6dd2ac5849ff60c800640ea814c
unknown <nikhil1983.be22@chitkara.edu.in>
2025-05-02 16:52:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>My React App</h1>
+        <h2>React implement with CI/CD</h2>
+        <h3>With Github Action</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
